f8d30f67659f482ec19c9f5fab973f76e3fdfe1a
seanlee1986 <coolio0212@hotmail.com>
2022-07-19 09:27:07 +0900

    remove backend

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "eshop-sean"        # my-s3-bucket
-    key    = "eshop/terraform.tfstate"
-    region = "ap-northeast-2"      # us-east-1, us-west-2 
-  }
-  required_version = ">=1.1.3"
-}
-
 provider "aws" {
   region = var.aws_region
 }
