8df58111fe80d95e0649ab0b03794c61d51bb7c4
seanlee1986 <coolio0212@hotmail.com>
2022-07-26 12:19:36 +0900

    depends_on apply

--- eks_module/eks-worker-nodes.tf
+++ eks_module/eks-worker-nodes.tf
@@ -60,6 +60,6 @@ resource "aws_eks_node_group" "terra" {
     aws_iam_role_policy_attachment.terra-node-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.terra-node-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.terra-node-AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.terra-node-AmazonEC2ContainerRegistryPowerUser
+    aws_iam_role_policy_attachment.terra-node-EC2InstanceProfileForImageBuilderECRContainerBuilds
   ]
 }
